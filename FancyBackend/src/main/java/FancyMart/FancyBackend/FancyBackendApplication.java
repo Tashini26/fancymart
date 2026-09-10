20924a95fc54900a70661b06ffda5519d6fbd0f0
UdulaPahasara <udulapahasara2@gmail.com>
2026-09-10 09:25:54 -0700

    initilize Backend

---
+++ FancyBackend/src/main/java/FancyMart/FancyBackend/FancyBackendApplication.java
@@ -0,0 +1,13 @@
+package FancyMart.FancyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyBackendApplication.class, args);
+	}
+
+}
